8fbd723a31eea7694007997ade0db2f7f2da72ac
Oscar Balcells <balcells.oscar@gmail.com>
2020-05-13 02:36:49 +0200

    Updates

---
+++ precache-manifest.29acf661ff0243927aa0b5f557343311.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "884332002f131f1b1d37229ba80df371",
+    "url": "/notas/index.html"
+  },
+  {
+    "revision": "28f10af89637106b3301",
+    "url": "/notas/static/css/2.35fcc229.chunk.css"
+  },
+  {
+    "revision": "9efb7d85be8748fe0320",
+    "url": "/notas/static/css/main.88c529da.chunk.css"
+  },
+  {
+    "revision": "28f10af89637106b3301",
+    "url": "/notas/static/js/2.b42a1b28.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/notas/static/js/2.b42a1b28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9efb7d85be8748fe0320",
+    "url": "/notas/static/js/main.d3c6c434.chunk.js"
+  },
+  {
+    "revision": "a9170ed60670f86d49c0",
+    "url": "/notas/static/js/runtime-main.75ac1a58.js"
+  }
+]);
